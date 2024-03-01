958f0db8957f86adbadfe9e67540ea8b354f5fb9
Stefan Agrigo Tagonsoft <stefanagrigo.tagonsoft@gmail.com>
2024-03-01 16:25:59 +0200

    meal_app with 2 tabs and favorites page

---
+++ lib/screens/meals.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widgets/meal_item.dart';
+import 'package:meal_app/screens/meals_details.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen(
+      {super.key,
+      this.title,
+      required this.meals,
+      required this.onToggleFavorite});
+
+  final String? title;
+  final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) =>
+            MealDetailsScreen(meal: meal, onToggleFavorite: onToggleFavorite),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Oh, nothing is here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Try select a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (context, meal) {
+            selectMeal(context, meal);
+          },
+        ),
+      );
+    }
+
+    if (title == null) {
+      return content;
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: content,
+    );
+  }
+}
